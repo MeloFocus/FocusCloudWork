7f8fc1bf0a006ce5de6724ee186680ad72f7fb34
melo <825178382@qq.com>
2020-02-02 14:15:16 +0800

    数据库

--- focus-framework-zuul/src/main/java/com/focus/zuul/config/WebMvcConfig.java
+++ focus-framework-zuul/src/main/java/com/focus/zuul/config/WebMvcConfig.java
@@ -2,15 +2,17 @@ package com.focus.zuul.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:8080","http://localhost:18600",
+                        "http://47.98.153.30:18600","113.134.36.204:18600")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                 .maxAge(3600)
                 .allowCredentials(true);
